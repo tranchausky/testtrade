db45f445d2cf4a9dc0ad9ffcbb85e9aff93da606
chau tran <tranchausky@gmail.com>
2021-02-06 18:43:31 +0700

    update version

--- auto-run/run-auto.js
+++ auto-run/run-auto.js
@@ -18,7 +18,7 @@ function setTimeoutAgain()
            //setPrice(numberSet)
        //}
        
-       if(se ==25){
+       if(se ==27){
            clog('Build')
            changeWay()
            
