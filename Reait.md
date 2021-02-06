45b63973ba692a4d334d1e3f3b2a9eabbea50f11
chau tran <tranchausky@gmail.com>
2021-02-06 14:16:10 +0700

    add test

---
+++ Reait.md
@@ -0,0 +1,17 @@
+Soure code
+https://github.com/tranchausky/testtrade/tree/staging
+
+server
+https://tranquil-fjord-14350.herokuapp.com/
+
+Run
+- file release encry
+js obfuscated https://obfuscator.io/
+
+- copy to js Run
+
+var fileref=document.createElement('script')
+fileref.setAttribute("type","text/javascript")
+fileref.setAttribute("src", filename)
+
+http://www.javascriptkit.com/javatutors/loadjavascriptcss.shtml
--- auto-run/run-auto.js
+++ auto-run/run-auto.js
@@ -1,29 +1,28 @@
 function setTimeoutAgain()
 {
-	
-	var t = setTimeout(function()
+    var t = setTimeout(function()
    {
-		
-	   var se = getSecond()
-	   //console.log('run-'+se)
-	   
-	   if(se ==4){
-		   setHistory()
-		   changeWay()
-	   }
-	   
-	   if(se ==7){
-		   var numberSet =getValueSet()
-		   setPrice(numberSet)
-	   }
-	   if(se ==8){
-		   //console.log(se)
-		   build()
-	   }
-	   if(se ==30){
-		   //console.log('30')
-	   }
-		setTimeoutAgain()
+
+       var se = getSecond()
+       //console.log('run-'+se)
+
+       if(se ==4){
+           setHistory()
+           changeWay()
+       }
+
+       if(se ==7){
+           var numberSet =getValueSet()
+           setPrice(numberSet)
+       }
+       if(se ==8){
+           //console.log(se)
+           build()
+       }
+       if(se ==30){
+           //console.log('30')
+       }
+        setTimeoutAgain()
    },1000);
 }
 var isWIN=true;
@@ -32,35 +31,35 @@ var tem={};
 tem.old=getMoney();
 tem.new=0;
 function reloadIsWin(){
-	tem.new=getMoney()
-	var status = false
-	if(tem.new >tem.old){
-		status=true
-	}
-	tem.old = tem.new
-	return status
+    tem.new=getMoney()
+    var status = false
+    if(tem.new >tem.old){
+        status=true
+    }
+    tem.old = tem.new
+    return status
 }
 function setPrice(number){
-	jQuery('#InputNumber').val(number).trigger('focus')
+    jQuery('#InputNumber').val(number).trigger('focus')
    jQuery('#rightContent .btnSuccess').trigger('focus')
 }
 function getMoney(){
-	var tien = jQuery('.balance b').text()
-	
-	tien= tien.replace(/,/g,'')
-	tien = parseFloat(tien.substring(1));
-	return tien
+    var tien = jQuery('.balance b').text()
+
+    tien= tien.replace(/,/g,'')
+    tien = parseFloat(tien.substring(1));
+    return tien
 }
 function build(){
-	var isOff = jQuery('#rightContent .btnSuccess').hasClass('colorDisable')
-	if(isOff == false){
-		jQuery('#rightContent .btnSuccess').trigger('click')
-	}
-	
-	//glb_whatWay check to up/down
-	
-	setPrice()
-	//console.log("is win: "+reloadIsWin())
+    var isOff = jQuery('#rightContent .btnSuccess').hasClass('colorDisable')
+    if(isOff == false){
+        jQuery('#rightContent .btnSuccess').trigger('click')
+    }
+
+    //glb_whatWay check to up/down
+
+    setPrice()
+    //console.log("is win: "+reloadIsWin())
 }
 
 
@@ -83,14 +82,14 @@ var clGreen='#31BAA0'
 var clRed='#FC5F5F'
 //true=green, false=red
 function colorAt(at){
-	var postion = $('.highcharts-series-group').eq(0).find('path').eq(at)
-	var attr = postion.attr('fill')
+    var postion = $('.highcharts-series-group').eq(0).find('path').eq(at)
+    var attr = postion.attr('fill')
 
-	if(attr==clRed){
-		false
-	}
-	return true;
-	//return attr;
+    if(attr==clRed){
+        return false
+    }
+    return true;
+    //return attr;
 }
 
 //status last win/lost
@@ -98,49 +97,49 @@ var atLastWin =false
 //total lost last
 var numberLastFalse = 0
 //value set auto
-var lostValueSet=[
-	0:1,
-	1:2,
-	3:4,
-	4:8,
-	5:16,
-	6:32,
-	7:64,
-	8:128,
-]
+var lostValueSet = {
+    0:1,
+    1:2,
+    3:4,
+    4:8,
+    5:16,
+    6:32,
+    7:64,
+    8:128
+}
 //get money back
 function getValueSet(){
-	var valueSet = 1;
-	if(atLastWin ==false){
-		valueSet = lostValueSet[numberLastFalse])
-	}
-	return valueSet
+    var valueSet = 1;
+    if(atLastWin ==false){
+        valueSet = lostValueSet[numberLastFalse]
+    }
+    return valueSet
 }
 
 //set value if lost/win
 function setHistory(){
-	//console.log(se)
+    //console.log(se)
    atLastWin = reloadIsWin()
    if(atLastWin ==false){
-	   numberLastFalse++
+       numberLastFalse++
    }else{
-	   numberLastFalse=0
+       numberLastFalse=0
    }
    //resert value, alot of lost, back to 0
    if(numberLastFalse == lostValueSet.length-1){
-	   numberLastFalse=0
+       numberLastFalse=0
    }
 }
 
 function changeWay(){
-	var way = true;
-	if(colorAt(0)==false && colorAt(1)==false && colorAt(2)==false){
-		way = false;
-	}
-	if(colorAt(0)==false && colorAt(1)==false){
-		way = true;
-	}
-	glb_whatWay =way
+    var way = true;
+    if(colorAt(0)==false && colorAt(1)==false && colorAt(2)==false){
+        way = false;
+    }
+    if(colorAt(0)==false && colorAt(1)==false){
+        way = true;
+    }
+    glb_whatWay =way
 }
 
 setTimeoutAgain()
